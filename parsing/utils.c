b6d6f922d34a979c64ca91bb58cf831291ffa1e0
Toony06 <tony.roman.pro@outlook.fr>
2025-11-13 13:16:04 +0100

    pars

--- parsing/utils.c
+++ parsing/utils.c
@@ -6,7 +6,7 @@
 /*   By: tony <tony@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 12:51:41 by tony              #+#    #+#             */
-/*   Updated: 2025/11/13 12:54:16 by tony             ###   ########.fr       */
+/*   Updated: 2025/11/13 13:15:46 by tony             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,59 @@ void    check_fd(int fd)
         close(fd);
         exite(1);
     }
+}
+
+int check_file(char *argv)
+{
+    if (ft_strncmp(argv + ft_strlen(argv) - 4, ".cub", 4))
+    {
+        printf("this file is not .cub\n");
+        exit(1);
+    }
+    return (0);
+}
+
+int count_lines(char *argv)
+{
+    int i;
+    int fd;
+    char    *lines;
+
+    i = 0;
+    fd = open(argv, O_RDONLY);
+    lines = get_next_line(fd);
+    while(lines)
+    {
+        i++;
+        free(lines);
+        lines = get_next_line(fd);
+    }
+    close (fd);
+    return (i);
+}
+
+void    print_map(char **argv)
+{
+    int i;
+
+    i = 0;
+    while(argv[i])
+    {
+        printf("%s\n", argv[i]);
+        i++;
+    }
+}
+
+int check_space(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+    {
+        if (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+            return (1);
+        i++;
+    }
+    return (0);
 }
